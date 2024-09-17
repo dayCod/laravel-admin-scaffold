cf8840303b320291605d289dc312786665ab82f1
wirandra.rembon <alayarahman07@gmail.com>
2024-09-17 08:51:15 +0700

    update: auth templating and media library migrations

---
+++ routes/web/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'login', 'as' => 'login.'], function () {
+
+    //
+
+});
+
+Route::group(['prefix' => 'register', 'as' => 'register.'], function () {
+
+    //
+
+});
--- routes/web/landing-page.php
+++ routes/web/landing-page.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'Global API');
+Route::get('/', function () {
+    return view('admin.index');
+});
