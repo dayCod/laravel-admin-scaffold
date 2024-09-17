1de8deb96deb4204857a7beeb68badc984ce9afe
wirandra.rembon <alayarahman07@gmail.com>
2024-09-17 08:26:16 +0700

    deps: admin assets and setup single page application

---
+++ resources/views/admin/layout/style.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
