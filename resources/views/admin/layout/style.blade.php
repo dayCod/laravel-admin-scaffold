c375986475614ee659c5f71622a43fcf662cfee4
wirandra.rembon <alayarahman07@gmail.com>
2024-09-21 19:35:22 +0700

    update: dashboard navbar title

--- resources/views/admin/layout/style.blade.php
+++ resources/views/admin/layout/style.blade.php
@@ -4,3 +4,6 @@
 <link rel="stylesheet" href="{{ asset('assets/vendors/iziToast/css/iziToast.min.css') }}">
 <link rel="stylesheet" href="https://cdn.datatables.net/2.1.6/css/dataTables.dataTables.min.css">
 <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
